ff4c866ed4dd651e4d4ed6f874718757977f9ee3
Sumedh Salvi <salvisumedh2396@gmail.com>
2020-12-21 11:10:57 +0530

    Spring basics brushup - Smart contact manager

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,43 @@
+console.log("This is a script file");
+
+var toggleSideBar = () => {
+	if ($(".sidebar").is(":visible")){
+        $(".sidebar").css("display", "none");
+        $(".content").css("margin-left", "10%");
+    }else{
+        $(".sidebar").css("display", "block");
+        $(".content").css("margin-left", "20%");
+    }
+};
+
+var search = () => {
+    //console.log("searching");
+
+    let query = $("#search-input").val();
+
+    if(query == ''){
+        $(".search-result").hide();
+    }else{
+        console.log(query);
+
+        let url = `http://localhost:8080/search/${query}`;
+
+        fetch(url).then((response) => {
+            return response.json();
+        })
+        .then((data) => { 
+            console.log(data);
+
+            let text = `<div class='list-group'>`
+
+            data.forEach(contact => {
+                text+= `<a href='/user/${contact.cId}/contact' class='list-group-item list-group-action'> ${contact.name} </a>`
+            });
+
+            text+=`</div>`;
+
+            $(".search-result").html(text);
+            $(".search-result").show();
+         });
+    }
+}
